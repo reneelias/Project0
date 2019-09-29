e743ae0583fee11cba9af3e83377aec723a1ee58
Rene Elias <reneelias03@gmail.com>
2019-09-28 19:53:49 -0700

    Initial Commit. Complete project.

---
+++ shopSmartV2.py
@@ -0,0 +1,63 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+from __future__ import print_function
+import shop
+
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+#    "Declare a variable to store the shop that will eventually"
+#    "be returned"
+    returnShop = None
+#    "Declare a variable to keep track of the first iteration"
+    i = 0
+            
+#    "Loop through each fruit shop"
+    for fruitShop in fruitShops:
+#        On the first iteration of the loop, the current shop
+#        is the one with the lowest total by default
+        if i == 0:
+            returnShop = fruitShop
+            i += 1
+        #If we are no longer on first iteration, check to see if
+        #the current shop's total is lower than our previous lowest
+        elif returnShop.getPriceOfOrder(orderList) > fruitShop.getPriceOfOrder(orderList):
+            returnShop = fruitShop
+        
+    return returnShop
+
+
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orders = [('apples', 1.0), ('oranges', 3.0)]
+    dir1 = {'apples': 2.0, 'oranges': 1.0}
+    shop1 = shop.FruitShop('shop1', dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2', dir2)
+    shops = [shop1, shop2]
+    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    orders = [('apples', 3.0)]
+    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
